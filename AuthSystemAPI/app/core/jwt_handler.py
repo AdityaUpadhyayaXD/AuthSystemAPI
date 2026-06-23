52e67c02a36d603ad1b52353259d1df85b2478c5
Aditya Upadhyaya <adityaupadhyaya23@gmail.com>
2026-06-23 17:44:59 +0530

    chore: initialize project virtual environment and install dependencies

---
+++ AuthSystemAPI/app/core/jwt_handler.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta, UTC
+from jose import jwt, JWTError
+
+SECRET_KEY = "your-super-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(UTC) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload.get("sub")
+
+    except JWTError:
+        return None
---
+++ AuthSystemAPI/app/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = (
+    "mysql+pymysql://root:@localhost/auth_system_api"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
