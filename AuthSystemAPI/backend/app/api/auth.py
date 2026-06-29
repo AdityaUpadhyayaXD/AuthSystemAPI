90a37ab418ffae78d1f151f5e3a0ebac3785cbbe
Aditya Upadhyaya <adityaupadhyaya23@gmail.com>
2026-06-29 11:38:33 +0530

    feat: implement auth router with registration and cookie-based login endpoints

--- AuthSystemAPI/backend/app/api/auth.py
+++ AuthSystemAPI/backend/app/api/auth.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
-
 from sqlalchemy.orm import Session
-
 from app.database import get_db
-
 from app.schemas.auth import RegisterRequest
-
 from app.services.auth_service import AuthService
+from fastapi import Response
+from app.schemas.auth import LoginRequest
+
+
 
 router = APIRouter(
     prefix="/auth",
@@ -35,4 +35,45 @@ def register(
 
     return {
         "message": "User registered successfully"
+    }
+
+@router.post("/login")
+def login(
+    request: LoginRequest,
+    response: Response,
+    db: Session = Depends(get_db)
+):
+
+    data, error = AuthService.login(
+        db,
+        request
+    )
+
+    if error:
+        raise HTTPException(
+            status_code=401,
+            detail=error
+        )
+
+    response.set_cookie(
+        key="access_token",
+        value=data["access_token"],
+        httponly=True,
+        secure=False,
+        samesite="lax",
+        max_age=60 * 30
+    )
+
+    response.set_cookie(
+        key="refresh_token",
+        value=data["refresh_token"],
+        httponly=True,
+        secure=False,
+        samesite="lax",
+        max_age=60 * 60 * 24 * 30
+    )
+
+    return {
+        "message": "Login successful",
+        "username": data["user"].username
     }
